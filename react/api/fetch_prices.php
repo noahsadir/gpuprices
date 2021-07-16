37875e215582216874d0b7d75ea86ef6e865bd46
noahsadir <development.noahsadir@gmail.com>
2021-07-15 21:14:27 -0400

    added filtering options to API

--- react/api/fetch_prices.php
+++ react/api/fetch_prices.php
@@ -1,28 +1,53 @@
 <?php
+//Location of prices list
 $pricesList = scandir('../../../gpu_prices/prices/');
-// DIR www/gpuprices.noahsadir.io/public_html/api/
-// --> www/gpuprices.noahsadir.io/public_html/
-// --> www/gpuprices.noahsadir.io/
-// --> www/
-// --> www/gpu_prices/
-// --> www/gpu_prices/prices/
+
+$fromTime = $_GET['from'];
+$interval = $_GET['interval'];
+
+//Set defaults for interval & from-time
+if (!isset($interval)) {
+  $interval = 0;
+} else {
+  $interval = intval($interval);
+}
+
+if (!isset($fromTime)) {
+  $fromTime = 0;
+} else {
+  $fromTime = intval($fromTime);
+}
 
 $output = '{';
 
-//TODO: Make operations safer; script makes lots of assumptions
+$lastTime = 0;
 
 //Go through each item in prices directory
 foreach ($pricesList as $priceFile){
   //Ignore dot files
   if ($priceFile != "." && $priceFile != ".."){
-    //Get contents of JSON file
-    $priceJson = file_get_contents('../../../gpu_prices/prices/'.$priceFile);
 
-    //Remove enclosing brackets
-    $priceJson = substr($priceJson, 1, -1);
+    //See if file name is numeric (should be milliseconds value)
+    if (intval(explode(".", $priceFile)[0])) {
 
-    //Add to output string
-    $output = $output.$priceJson.',';
+      //Get time value of file
+      $timeValue = intval(explode(".", $priceFile)[0]);
+
+      //Check if time value matches interval and start date criteria
+      if ($lastTime + $interval <= $timeValue && $timeValue >= $fromTime) {
+
+        //Get contents of JSON file
+        $priceJson = file_get_contents('../../../gpu_prices/prices/'.$priceFile);
+
+        //Remove enclosing brackets
+        $priceJson = substr($priceJson, 1, -1);
+
+        //Add to output string
+        $output = $output.$priceJson.',';
+
+        $lastTime = $timeValue;
+      }
+    }
   }
 }
 
@@ -33,6 +58,7 @@ if (substr($output,-1) == ","){
 
 //Close string and echo as JSON
 $output = $output.'}';
+
 header('Content-Type: application/json');
 echo $output;
  ?>
